49c6535d876c6e68bbc4e3edfd66ff4739a2a09c
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-06-27 22:01:51 +0100

    rewrite updates

--- rewrite/lws.config.js
+++ rewrite/lws.config.js
@@ -6,5 +6,6 @@ module.exports = {
     { from: '/gov', to: 'https://www.gov.uk' },
     { from: '/:user/repos/:name', to: 'https://api.github.com/repos/:user/:name' },
     { from: '/img1/(.*)', to: 'https://img.theculturetrip.com/$1' }
-  ]
+  ],
+  stack: [ 'get-config.js', 'rewrite', 'static' ]
 }
