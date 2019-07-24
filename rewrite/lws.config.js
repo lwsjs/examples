2f12b52f338ace5cc21681db1d9017d5c4c332e9
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-07-24 20:42:39 +0100

    rewrite

--- rewrite/lws.config.js
+++ rewrite/lws.config.js
@@ -6,7 +6,25 @@ module.exports = {
     { from: '/gov', to: 'https://www.gov.uk' },
     { from: '/:user/repos/:name', to: 'https://api.github.com/repos/:user/:name' },
     { from: '/img1/(.*)', to: 'https://img.theculturetrip.com/$1' },
-    { from: '/ws/(.*)', to: 'http://127.0.0.1:8100/$1' }
+    { from: '/ws/(.*)', to: 'http://127.0.0.1:8100/$1' },
+    { from: '/posts', to: 'http://jsonplaceholder.typicode.com/posts' },
+    { from: '/images', to: 'http://jsonplaceholder.typicode.com/posts' }
   ],
-  stack: [ 'get-config.js', 'rewrite', 'static' ]
+  stack: [
+      'lws-basic-auth',
+      'lws-body-parser',
+      'lws-request-monitor',
+      'lws-log',
+      'lws-cors',
+      'lws-json',
+      'lws-compress',
+      'lws-rewrite',
+      'lws-blacklist',
+      'lws-conditional-get',
+      'lws-mime',
+      'lws-range',
+      'lws-spa',
+      'lws-static',
+    'lws-index'
+    ]
 }
