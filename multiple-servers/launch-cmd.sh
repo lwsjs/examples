0fd480fd7610f231003788775de1bfe61d4d3fa2
Lloyd Brookes <lloyd.brookes@gmail.com>
2024-08-13 13:06:49 +0100

    multiple servers

---
+++ multiple-servers/launch-cmd.sh
@@ -0,0 +1,1 @@
+lws --app static --port 8000 --static.root.string one/ --app static --port 8010 --static.root.string two/
---
+++ multiple-servers/lws.config.mjs
@@ -0,0 +1,10 @@
+export default [
+  {
+    port: 8000,
+    directory: 'one'
+  },
+  {
+    port: 8010,
+    directory: 'two'
+  }
+]
